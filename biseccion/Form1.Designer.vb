05a3fe42c1c8dd215502b86c7dabd8f8f75f8998
JuanChavezX <37625427+JuanChavezX@users.noreply.github.com>
2022-03-18 20:31:06 -0600

    Add files via upload

---
+++ biseccion/Form1.Designer.vb
@@ -0,0 +1,292 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.tf = New System.Windows.Forms.TextBox()
+        Me.ta = New System.Windows.Forms.TextBox()
+        Me.tb = New System.Windows.Forms.TextBox()
+        Me.tc = New System.Windows.Forms.TextBox()
+        Me.Calcular = New System.Windows.Forms.Button()
+        Me.Salir = New System.Windows.Forms.Button()
+        Me.Salida = New System.Windows.Forms.DataGridView()
+        Me.Column1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column3 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column4 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column5 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column6 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column7 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column8 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.tr = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        CType(Me.Salida, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'tf
+        '
+        Me.tf.Location = New System.Drawing.Point(238, 78)
+        Me.tf.Name = "tf"
+        Me.tf.Size = New System.Drawing.Size(100, 23)
+        Me.tf.TabIndex = 0
+        '
+        'ta
+        '
+        Me.ta.Location = New System.Drawing.Point(238, 118)
+        Me.ta.Name = "ta"
+        Me.ta.Size = New System.Drawing.Size(100, 23)
+        Me.ta.TabIndex = 1
+        '
+        'tb
+        '
+        Me.tb.Location = New System.Drawing.Point(238, 165)
+        Me.tb.Name = "tb"
+        Me.tb.Size = New System.Drawing.Size(100, 23)
+        Me.tb.TabIndex = 2
+        '
+        'tc
+        '
+        Me.tc.Location = New System.Drawing.Point(238, 210)
+        Me.tc.Name = "tc"
+        Me.tc.Size = New System.Drawing.Size(100, 23)
+        Me.tc.TabIndex = 3
+        '
+        'Calcular
+        '
+        Me.Calcular.BackColor = System.Drawing.SystemColors.Highlight
+        Me.Calcular.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.Calcular.Cursor = System.Windows.Forms.Cursors.Default
+        Me.Calcular.Font = New System.Drawing.Font("Arial Narrow", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Calcular.Location = New System.Drawing.Point(437, 256)
+        Me.Calcular.Name = "Calcular"
+        Me.Calcular.Size = New System.Drawing.Size(106, 50)
+        Me.Calcular.TabIndex = 4
+        Me.Calcular.Text = "&Calcular"
+        Me.Calcular.UseVisualStyleBackColor = False
+        '
+        'Salir
+        '
+        Me.Salir.BackColor = System.Drawing.SystemColors.Highlight
+        Me.Salir.Font = New System.Drawing.Font("Arial Narrow", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Salir.Location = New System.Drawing.Point(554, 256)
+        Me.Salir.Name = "Salir"
+        Me.Salir.Size = New System.Drawing.Size(114, 50)
+        Me.Salir.TabIndex = 5
+        Me.Salir.Text = "&Salir"
+        Me.Salir.UseVisualStyleBackColor = False
+        '
+        'Salida
+        '
+        Me.Salida.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.Salida.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column1, Me.Column2, Me.Column3, Me.Column4, Me.Column5, Me.Column6, Me.Column7, Me.Column8})
+        Me.Salida.Location = New System.Drawing.Point(58, 337)
+        Me.Salida.Name = "Salida"
+        Me.Salida.RowTemplate.Height = 25
+        Me.Salida.Size = New System.Drawing.Size(838, 150)
+        Me.Salida.TabIndex = 6
+        '
+        'Column1
+        '
+        Me.Column1.HeaderText = "Indice"
+        Me.Column1.Name = "Column1"
+        '
+        'Column2
+        '
+        Me.Column2.HeaderText = "a"
+        Me.Column2.Name = "Column2"
+        '
+        'Column3
+        '
+        Me.Column3.HeaderText = "xi"
+        Me.Column3.Name = "Column3"
+        '
+        'Column4
+        '
+        Me.Column4.HeaderText = "b"
+        Me.Column4.Name = "Column4"
+        '
+        'Column5
+        '
+        Me.Column5.HeaderText = "f(a)"
+        Me.Column5.Name = "Column5"
+        '
+        'Column6
+        '
+        Me.Column6.HeaderText = "f(xi)"
+        Me.Column6.Name = "Column6"
+        '
+        'Column7
+        '
+        Me.Column7.HeaderText = "f(b)"
+        Me.Column7.Name = "Column7"
+        '
+        'Column8
+        '
+        Me.Column8.HeaderText = "error"
+        Me.Column8.Name = "Column8"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(127, 81)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(105, 15)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "Dame una funcion"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(149, 126)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(47, 15)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "Dame a"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(149, 168)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(48, 15)
+        Me.Label3.TabIndex = 9
+        Me.Label3.Text = "Dame b"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(149, 213)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(69, 15)
+        Me.Label4.TabIndex = 10
+        Me.Label4.Text = "Dame cifras"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Impact", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label5.Location = New System.Drawing.Point(320, 25)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(299, 39)
+        Me.Label5.TabIndex = 11
+        Me.Label5.Text = "METODO DE BISECCION"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.biseccion.My.Resources.Resources.biseccionformrecursiva
+        Me.PictureBox1.Location = New System.Drawing.Point(482, 90)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(302, 98)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 12
+        Me.PictureBox1.TabStop = False
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Arial Narrow", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label6.Location = New System.Drawing.Point(554, 191)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(170, 29)
+        Me.Label6.TabIndex = 13
+        Me.Label6.Text = "Formula recursiva"
+        '
+        'tr
+        '
+        Me.tr.Location = New System.Drawing.Point(238, 516)
+        Me.tr.Name = "tr"
+        Me.tr.Size = New System.Drawing.Size(100, 23)
+        Me.tr.TabIndex = 14
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(112, 519)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(120, 15)
+        Me.Label7.TabIndex = 15
+        Me.Label7.Text = "La raiz aproximada es"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.Window
+        Me.ClientSize = New System.Drawing.Size(948, 577)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.tr)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Salida)
+        Me.Controls.Add(Me.Salir)
+        Me.Controls.Add(Me.Calcular)
+        Me.Controls.Add(Me.tc)
+        Me.Controls.Add(Me.tb)
+        Me.Controls.Add(Me.ta)
+        Me.Controls.Add(Me.tf)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.Salida, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents tf As TextBox
+    Friend WithEvents ta As TextBox
+    Friend WithEvents tb As TextBox
+    Friend WithEvents tc As TextBox
+    Friend WithEvents Calcular As Button
+    Friend WithEvents Salir As Button
+    Friend WithEvents Salida As DataGridView
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Column1 As DataGridViewTextBoxColumn
+    Friend WithEvents Column2 As DataGridViewTextBoxColumn
+    Friend WithEvents Column3 As DataGridViewTextBoxColumn
+    Friend WithEvents Column4 As DataGridViewTextBoxColumn
+    Friend WithEvents Column5 As DataGridViewTextBoxColumn
+    Friend WithEvents Column6 As DataGridViewTextBoxColumn
+    Friend WithEvents Column7 As DataGridViewTextBoxColumn
+    Friend WithEvents Column8 As DataGridViewTextBoxColumn
+    Friend WithEvents Label5 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents tr As TextBox
+    Friend WithEvents Label7 As Label
+End Class
